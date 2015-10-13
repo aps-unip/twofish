16ce39d217827c5e22ead7bd35245f2d54a59a0c
Jones Vitolo <jsvitolo@gmail.com>
2015-10-13 15:55:05 -0300

    Correcao no codigo

--- encrypt.rb
+++ encrypt.rb
@@ -4,8 +4,15 @@ require 'base64'
 require_relative 'generate_key'
 require_relative 'generate_text'
 
-key = @rand_key
+file = File.open("key", "r")
+key = file.read
+
 tf = Twofish.new(key, :mode => :ecb, :padding => :zero_byte)
-texto_cripto = tf.encrypt('@texto')
+
+file2 = File.open("texto", "r")
+texto = file2.read
+texto_cripto = tf.encrypt(texto)
 
 base = Base64.encode64(texto_cripto)
+
+puts base
